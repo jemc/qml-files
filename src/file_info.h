bd632c91bd5d08366135326708f5ae48b3f6c403
Joe McIlvain <joe.eli.mac@gmail.com>
2014-02-04 00:19:20 -0800

    Added trivial non-static QFileInfo methods (untested)
    --HG--
    extra : amend_source : cdbde94d524a78c2641909856fad60fd9ee6fd61

--- src/file_info.h
+++ src/file_info.h
@@ -13,12 +13,139 @@ class FileInfo : public QObject
 public:
     static QObject* qmlAttachedProperties(QObject* object);
     
-    FileInfo() { qdir = QFileInfo(""); };
-    FileInfo(const QFileInfo& dir) { qdir = QFileInfo(dir); };
+    FileInfo() { qfileinfo = QFileInfo(""); };
+    FileInfo(const QFileInfo& info) { qfileinfo = QFileInfo(info); };
     
-    QFileInfo qdir;
+    QFileInfo qfileinfo;
     
 public slots:
+    // QDir absoluteDir()
+    
+    QString absoluteFilePath()
+    { return qfileinfo.absoluteFilePath(); };
+    
+    QString absolutePath()
+    { return qfileinfo.absolutePath(); };
+    
+    QString baseName()
+    { return qfileinfo.baseName(); };
+    
+    QString bundleName()
+    { return qfileinfo.bundleName(); };
+    
+    bool caching()
+    { return qfileinfo.caching(); };
+    
+    QString canonicalFilePath()
+    { return qfileinfo.canonicalFilePath(); };
+    
+    QString canonicalPath()
+    { return qfileinfo.canonicalPath(); };
+    
+    QString completeBaseName()
+    { return qfileinfo.completeBaseName(); };
+    
+    QString completeSuffix()
+    { return qfileinfo.completeSuffix(); };
+    
+    // QDateTime created()
+    
+    // QDir dir()
+    
+    bool exists()
+    { return qfileinfo.exists(); };
+    
+    QString fileName()
+    { return qfileinfo.fileName(); };
+    
+    QString filePath()
+    { return qfileinfo.filePath(); };
+    
+    QString group()
+    { return qfileinfo.group(); };
+    
+    uint groupId()
+    { return qfileinfo.groupId(); };
+    
+    bool isAbsolute()
+    { return qfileinfo.isAbsolute(); };
+    
+    bool isBundle()
+    { return qfileinfo.isBundle(); };
+    
+    bool isDir()
+    { return qfileinfo.isDir(); };
+    
+    bool isExecutable()
+    { return qfileinfo.isExecutable(); };
+    
+    bool isFile()
+    { return qfileinfo.isFile(); };
+    
+    bool isHidden()
+    { return qfileinfo.isHidden(); };
+    
+    bool isNativePath()
+    { return qfileinfo.isNativePath(); };
+    
+    bool isReadable()
+    { return qfileinfo.isReadable(); };
+    
+    bool isRelative()
+    { return qfileinfo.isRelative(); };
+    
+    bool isRoot()
+    { return qfileinfo.isRoot(); };
+    
+    bool isSymLink()
+    { return qfileinfo.isSymLink(); };
+    
+    bool isWritable()
+    { return qfileinfo.isWritable(); };
+    
+    // QDateTime lastModified()
+    
+    // QDateTime lastRead()
+    
+    bool makeAbsolute()
+    { return qfileinfo.makeAbsolute(); };
+    
+    QString owner()
+    { return qfileinfo.owner(); };
+    
+    uint ownerId()
+    { return qfileinfo.ownerId(); };
+    
+    QString path()
+    { return qfileinfo.path(); };
+    
+    // bool permission(QFile::Permissions permissions)
+    
+    // QFile::Permissions permissions()
+    
+    void refresh()
+    { return qfileinfo.refresh(); };
+    
+    void setCaching(bool enable)
+    { return qfileinfo.setCaching(enable); };
+    
+    void setFile(const QString & file)
+    { return qfileinfo.setFile(file); };
+    
+    // void setFile(const QFile & file)
+    
+    // void setFile(const QDir & dir, const QString & file)
+    
+    qint64 size()
+    { return qfileinfo.size(); };
+    
+    QString suffix()
+    { return qfileinfo.suffix(); };
+    
+    // void swap(QFileInfo & other)
+    
+    QString symLinkTarget()
+    { return qfileinfo.symLinkTarget(); };
     
 };
 
